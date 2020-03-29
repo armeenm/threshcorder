dd943107682c380ceb1a7d88f530621fc8059f94
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-03-29 12:14:36 -0500

    Add RMS capabilities and refactor helper functions into header

--- threshcorder/main.cpp
+++ threshcorder/main.cpp
@@ -1,17 +1,9 @@
-#include <alsa/asoundlib.h>
-#include <array>
-#include <cstdio>
-#include <fmt/core.h>
-#include <memory>
-#include <optional>
+#include "threshcorder/util.h"
+
+#include <chrono>
+#include <iostream>
 
 using namespace std::literals;
-
-auto get_handle(std::string_view device, snd_pcm_format_t format, unsigned int rate) -> snd_pcm_t*;
-
-template <snd_pcm_uframes_t frames>
-auto listen(snd_pcm_t* const handle)
-    -> std::optional<std::pair<std::array<std::uint8_t, frames>, snd_pcm_uframes_t>>;
 
 auto main(int const argc, char const* const* const argv) -> int {
   if (argc != 2) {
@@ -19,93 +11,35 @@ auto main(int const argc, char const* const* const argv) -> int {
     return -1;
   }
 
+  auto constexpr format = SND_PCM_FORMAT_S16_LE;
+  auto constexpr rate = 44100u;
+  auto constexpr buf_size = rate / 8;
+
   auto& dev_name = argv[1];
 
-  auto constexpr format = SND_PCM_FORMAT_S16_LE;
-
-  auto handle = get_handle(dev_name, format, 44100u);
+  auto handle = get_handle(dev_name, format, rate);
   if (!handle) {
     fmt::print(stderr, "Failed to open and configure device {}\n", dev_name);
     return -1;
   }
 
   while (true) {
-    auto const [data, count] = listen<128ul>(handle).value();
+    auto const start = std::chrono::high_resolution_clock::now();
 
-    for (snd_pcm_uframes_t j = 0; j < count; ++j)
-      fmt::print("{} ", data[j]);
+    auto const [data, count] = listen<buf_size>(handle).value();
+    auto const rms_val = rms(data.begin(), data.begin() + count);
+
+    auto const stop = std::chrono::high_resolution_clock::now();
+
+    fmt::print("RMS: {}, Count: {}", rms_val, count);
+    fmt::print(", Time: {}ms, Samples: ",
+               std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count());
+
+    for (auto i = 0; i < 10; ++i)
+      fmt::print("{} ", data[i]);
+
+    std::cout << '\n';
   }
 
   snd_pcm_close(handle);
-}
-
-template <snd_pcm_uframes_t frames>
-auto listen(snd_pcm_t* const handle)
-    -> std::optional<std::pair<std::array<std::uint8_t, frames>, snd_pcm_uframes_t>> {
-
-  std::array<std::uint8_t, frames> buf;
-
-  long int err;
-  if ((err = snd_pcm_readi(handle, buf.data(), frames)) != frames) {
-    fmt::print(stderr, "Read from audio interface failed ({})\n",
-               snd_strerror(static_cast<int>(err)));
-    return std::nullopt;
-  }
-
-  return std::make_pair(buf, err);
-}
-
-auto get_handle(std::string_view device, snd_pcm_format_t format, unsigned int rate) -> snd_pcm_t* {
-  int err;
-  snd_pcm_t* handle;
-  snd_pcm_hw_params_t* hw_params;
-
-  if ((err = snd_pcm_open(&handle, device.data(), SND_PCM_STREAM_CAPTURE, 0)) < 0) {
-    fmt::print(stderr, "Cannot open audio device {} ({})\n", device.data(), snd_strerror(err));
-    return nullptr;
-  }
-
-  if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) {
-    fmt::print(stderr, "Cannot allocate hardware parameter structure ({})\n", snd_strerror(err));
-    return nullptr;
-  }
-
-  if ((err = snd_pcm_hw_params_any(handle, hw_params)) < 0) {
-    fmt::print(stderr, "Cannot initialize hardware parameter structure ({})\n", snd_strerror(err));
-    return nullptr;
-  }
-
-  if ((err = snd_pcm_hw_params_set_access(handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
-    fmt::print(stderr, "Cannot set access type ({})\n", snd_strerror(err));
-    return nullptr;
-  }
-
-  if ((err = snd_pcm_hw_params_set_format(handle, hw_params, format)) < 0) {
-    fmt::print(stderr, "Cannot set sample format ({})\n", snd_strerror(err));
-    return nullptr;
-  }
-
-  if ((err = snd_pcm_hw_params_set_rate_near(handle, hw_params, &rate, 0)) < 0) {
-    fmt::print(stderr, "Cannot set sample rate ({})\n", snd_strerror(err));
-    return nullptr;
-  }
-
-  if ((err = snd_pcm_hw_params_set_channels(handle, hw_params, 1)) < 0) {
-    fmt::print(stderr, "Cannot set channel count ({})\n", snd_strerror(err));
-    return nullptr;
-  }
-
-  if ((err = snd_pcm_hw_params(handle, hw_params)) < 0) {
-    fmt::print(stderr, "Cannot set parameters ({})\n", snd_strerror(err));
-    return nullptr;
-  }
-
-  snd_pcm_hw_params_free(hw_params);
-
-  if ((err = snd_pcm_prepare(handle)) < 0) {
-    fmt::print(stderr, "Cannot prepare audio interface for use ({})\n", snd_strerror(err));
-    return nullptr;
-  }
-
-  return handle;
 }
---
+++ threshcorder/util.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <alsa/asoundlib.h>
+#include <array>
+#include <cmath>
+#include <fmt/core.h>
+#include <memory>
+#include <numeric>
+#include <optional>
+
+using sample_t = std::int16_t;
+using fat_t = std::int64_t;
+
+auto get_handle(std::string_view device, snd_pcm_format_t format, unsigned int rate) -> snd_pcm_t* {
+  int err;
+  snd_pcm_t* handle;
+  snd_pcm_hw_params_t* hw_params;
+
+  if ((err = snd_pcm_open(&handle, device.data(), SND_PCM_STREAM_CAPTURE, 0)) < 0) {
+    fmt::print(stderr, "Cannot open audio device {} ({})\n", device.data(), snd_strerror(err));
+    return nullptr;
+  }
+
+  if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) {
+    fmt::print(stderr, "Cannot allocate hardware parameter structure ({})\n", snd_strerror(err));
+    return nullptr;
+  }
+
+  if ((err = snd_pcm_hw_params_any(handle, hw_params)) < 0) {
+    fmt::print(stderr, "Cannot initialize hardware parameter structure ({})\n", snd_strerror(err));
+    return nullptr;
+  }
+
+  if ((err = snd_pcm_hw_params_set_access(handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+    fmt::print(stderr, "Cannot set access type ({})\n", snd_strerror(err));
+    return nullptr;
+  }
+
+  if ((err = snd_pcm_hw_params_set_format(handle, hw_params, format)) < 0) {
+    fmt::print(stderr, "Cannot set sample format ({})\n", snd_strerror(err));
+    return nullptr;
+  }
+
+  if ((err = snd_pcm_hw_params_set_rate_near(handle, hw_params, &rate, 0)) < 0) {
+    fmt::print(stderr, "Cannot set sample rate ({})\n", snd_strerror(err));
+    return nullptr;
+  }
+
+  if ((err = snd_pcm_hw_params_set_channels(handle, hw_params, 1)) < 0) {
+    fmt::print(stderr, "Cannot set channel count ({})\n", snd_strerror(err));
+    return nullptr;
+  }
+
+  if ((err = snd_pcm_hw_params(handle, hw_params)) < 0) {
+    fmt::print(stderr, "Cannot set parameters ({})\n", snd_strerror(err));
+    return nullptr;
+  }
+
+  snd_pcm_hw_params_free(hw_params);
+
+  if ((err = snd_pcm_prepare(handle)) < 0) {
+    fmt::print(stderr, "Cannot prepare audio interface for use ({})\n", snd_strerror(err));
+    return nullptr;
+  }
+
+  return handle;
+}
+
+template <snd_pcm_uframes_t frames>
+auto listen(snd_pcm_t* const handle)
+    -> std::optional<std::pair<std::array<sample_t, frames>, snd_pcm_uframes_t>> {
+
+  std::array<sample_t, frames> buf;
+
+  long int err;
+  if ((err = snd_pcm_readi(handle, buf.data(), frames)) != frames) {
+    fmt::print(stderr, "Read from audio interface failed ({})\n",
+               snd_strerror(static_cast<int>(err)));
+    return std::nullopt;
+  }
+
+  return std::make_pair(buf, err);
+}
+
+template <typename It> auto rms(It&& begin, It&& end) -> float {
+  using namespace std::literals;
+  using value_type = typename std::iterator_traits<It>::value_type;
+
+  auto constexpr square_add = [](value_type a, value_type b) {
+    auto const a_fat = fat_t{a};
+    auto const b_fat = fat_t{b};
+
+    return (a_fat * a_fat) + (b_fat * b_fat);
+  };
+
+  auto const count = (end - begin);
+
+  // fmt::print("Count: {}, Size: {}; ", count, sizeof(value_type));
+
+  auto const square =
+      std::accumulate(std::forward<It>(begin), std::forward<It>(end), value_type{0}, square_add);
+  auto const mean_square = static_cast<float>(square) / static_cast<float>(count);
+
+  fmt::print("Square: {}, Mean Square: {}; ", square, mean_square);
+
+  return std::sqrt(std::abs(mean_square));
+}
